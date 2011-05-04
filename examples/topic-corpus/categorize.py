e03d851a51b0177fd72638f278663de06c410a79
Olivier Grisel <olivier.grisel@ensta.org>
2011-05-04 19:46:39 +0200

    checkpointing work in progress on goodies stuff for easy solr integration

---
+++ examples/topic-corpus/categorize.py
@@ -0,0 +1,56 @@
+"""Simple script to perform a similarity query on a Solr index of topic
+
+Solr is expected to have the default schema.xml from the example folder
+with the additional field to store the abstracts of the topics:
+
+    <field name="abstract" type="textgen" indexed="true"
+      stored="false" termVectors="true"/>
+
+You can install sunburnt (the solr / python connector) with::
+
+    $ pip install sunburnt
+
+"""
+
+import os
+import sys
+import uuid
+import sunburnt
+
+class MoreLikeThisDocument(object):
+    """Transient document to get indexed to be able to do a similarity query"""
+
+    def __init__(self, text):
+        self.id = uuid.uuid4().get_hex()
+        self.type = "mlt_query_document"
+        self.text = text
+
+
+def categorize(schema, text):
+    q = MoreLikeThisDocument(text)
+    solr = sunburnt.SolrInterface("http://localhost:8983/solr", schema)
+    solr.add(q)
+    solr.commit()
+    try:
+        print "query id:", q.id
+        mlt_query = solr.query(id=q.id).mlt("text")
+        mlt_results = mlt_query.execute().more_like_these
+        if q.id in mlt_results:
+            return [d['id'] for d in mlt_results[q.id].docs]
+        else:
+            print "ERROR: query document with id='%s' not found" % q.id
+            return []
+    finally:
+        solr.delete(q)
+        solr.commit()
+
+
+if __name__ == "__main__":
+
+    schema = sys.argv[1]
+    document = sys.argv[2]
+    if os.path.exists(document):
+        document = open(document).read()
+
+    for topic in categorize(schema, document):
+        print topic
